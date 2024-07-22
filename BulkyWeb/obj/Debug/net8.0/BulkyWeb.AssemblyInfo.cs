7ae966db26305e0df55af69550a6e5388b56e1af
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-07-22 22:14:41 +0400

    small

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abdc49f06e1ac3b265fe12c28191e1965c40665c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f8339e3814d7c32aec0666c3e72c32cce00435")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
