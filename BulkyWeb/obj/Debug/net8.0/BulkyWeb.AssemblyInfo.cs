56e25dcd9dad088ebc456e844f0054a2102c759c
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-07-23 23:07:49 +0400

    Served side Validations

--- BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
+++ BulkyWeb/obj/Debug/net8.0/BulkyWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f19f6da4860a66b98cd00c9b08ae0f1545b4e237")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5912b10fe21422f67715639d663cec177b5383d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BulkyWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
