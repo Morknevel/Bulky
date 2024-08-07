42033f5294096d687ee8b9dc83bfc6161464784f
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-08-08 01:12:33 +0400

    categoryUI

---
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using BulkyWebRazor_Temp.Data;
+using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazor_Temp.Pages.Categories;
+
+public class Create : PageModel
+{
+    private readonly ApplicationDbContext _db;
+    public Category Category { get; set; }
+
+    public Create(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+    public void OnGet()
+    {
+    }
+}
