fb3f2ad5b66b2eec4786fc7c9cfbde66349e9ca8
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-08-08 16:25:25 +0400

    create category post handler

--- BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ BulkyWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using BulkyWebRazor_Temp.Data;
 using BulkyWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace BulkyWebRazor_Temp.Pages.Categories;
@@ -7,13 +8,22 @@ namespace BulkyWebRazor_Temp.Pages.Categories;
 public class Create : PageModel
 {
     private readonly ApplicationDbContext _db;
+    [BindProperty]
     public Category Category { get; set; }
 
     public Create(ApplicationDbContext db)
     {
         _db = db;
     }
+
     public void OnGet()
     {
     }
+    public IActionResult OnPost()
+    {
+        _db.Categories.Add(Category);
+        _db.SaveChanges();
+        return RedirectToPage("Index");
+    }
+
 }
