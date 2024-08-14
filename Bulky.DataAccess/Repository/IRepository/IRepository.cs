1595825fb298fee767fe06abf5e04922fac203b1
Morknevel <154018153+Morknevel@users.noreply.github.com>
2024-08-14 23:16:12 +0400

    implemenet Repository interface

--- Bulky.DataAccess/Repository/IRepository/IRepository.cs
+++ Bulky.DataAccess/Repository/IRepository/IRepository.cs
@@ -4,9 +4,9 @@ namespace Bulky.DataAccess.IRepository;
 
 public interface IRepository<T> where T : class
 {
-    IEnumerable<T> getAll();
+    IEnumerable<T>GetAll();
     T Get(Expression<Func<T, bool>> filter);
-    T Add(T entity);
-    T Remove(T entity);
+    void Add(T entity);
+    void Remove(T entity);
     void RemoveRange(IEnumerable<T> entity);
 }
